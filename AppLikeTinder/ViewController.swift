5f253089c939933361f7b4f1c400dfe79da8c268
Marko Budal <markobudal@gmail.com>
2015-08-30 11:52:53 +0200

    Initial Commit

---
+++ AppLikeTinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppLikeTinder
+//
+//  Created by Marko Budal on 30/08/15.
+//  Copyright (c) 2015 Marko Budal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
